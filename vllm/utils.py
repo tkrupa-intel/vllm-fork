16b55577160e38b64bc79b1a65369abe2a3460a8
Konrad Zawora <kzawora@habana.ai>
2023-12-21 17:34:09 +0000

    Fix post-merge pinned memory segfaults

--- vllm/utils.py
+++ vllm/utils.py
@@ -59,3 +59,7 @@ def get_open_port():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("", 0))
         return s.getsockname()[1]
+
+
+def is_hpu() -> bool:
+    return getattr(torch, 'hpu', None) is not None and torch.hpu.is_available()
