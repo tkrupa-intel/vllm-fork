2b6ec4e3438b131cbc211e743aa427a655cb9bb7
Konrad Zawora <kzawora@habana.ai>
2023-12-21 17:43:01 +0000

    Re-enable sequence decode

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -425,7 +425,7 @@ class LLMEngine:
             child_seqs.append((parent, parent))
 
         for seq, _ in child_seqs:
-            # self._decode_sequence(seq, seq_group.sampling_params)
+            self._decode_sequence(seq, seq_group.sampling_params)
             self._check_stop(seq, seq_group.sampling_params)
 
         # Non-beam search case
