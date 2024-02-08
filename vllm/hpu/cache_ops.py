586bf1b6db0661d24f113e57b13a1c546b4cbcde
Mikhail Dvoretckii <mdvoretckii@habana.ai>
2024-02-08 18:32:21 +0200

    Enable cache ops for beam search

--- vllm/hpu/cache_ops.py
+++ vllm/hpu/cache_ops.py
@@ -64,6 +64,34 @@ def reshape_and_cache(key, value, key_cache, value_cache, slot_mapping, is_promp
         # print(key_cache.data_ptr(), key_cache.shape)
 
 
+def swap_blocks(src, dst, block_mapping):
+    #print(f'swap_blocks: {block_mapping}')
+    index_src = torch.zeros((1,), dtype=torch.int32, device=key_caches[0].device)
+    index_dst = torch.zeros((1,), dtype=torch.int32, device=key_caches[0].device)
+    for src_idx, dst_idx in block_mapping.items():
+        index_src[0] = src_idx
+        index_dst[0] = dst_idx
+        dst.index_put_([index_dst], src.index_select(0, index_src))
+        if dst.device.type == 'hpu':
+            htorch.core.mark_step()
+            torch.hpu.synchronize()
+
+
+def copy_blocks(key_caches, value_caches, block_mapping):
+    #print(f'copy_blocks: {block_mapping}')
+    index_src = torch.zeros((1,), dtype=torch.int32, device=key_caches[0].device)
+    index_dst = torch.zeros((1,), dtype=torch.int32, device=key_caches[0].device)
+    for src, dsts in block_mapping.items():
+        index_src[0] = src
+        for dst in dsts:
+            index_dst[0] = dst
+            for key_cache in key_caches:
+                key_cache.index_copy_(0, index_dst, key_cache.index_select(0, index_src))
+            for value_cache in value_caches:
+                value_cache.index_copy_(0, index_dst, value_cache.index_select(0, index_src))
+        if key_caches[0].device.type == 'hpu':
+            htorch.core.mark_step()
+
 
 '''
 def create_cache_view(
